43d3e7ee9825b8400b92cf4943e149eaeb182b12
Davi dos Santos Passos <fenandosouzamelo419@gmail.com>
2025-05-26 22:06:27 -0300

    criação de uma biblioteca para fazer log

---
+++ src/log.lua
@@ -0,0 +1,42 @@
+local log = {
+    _messages = {},
+    show = function ()
+        for i, v in ipairs(log._messages) do
+            print(v)
+        end 
+        print("Total messages: " .. #log._messages)	
+    end,
+    save = function ()
+        local base = "/src/logs/log"
+
+        local file = io.open(base, "w")
+        if not file then
+            print("Error opening file for writing: " .. filename)
+            return
+        end
+
+        for i, v in ipairs(log._messages) do
+            file:write(v .. "\n")
+        end
+
+        file:close()
+        print("Log saved")
+    end,
+    add = function (...)
+        local message = ""
+
+        for i, v in ipairs({...}) do
+            message = message .. " " .. tostring(v)
+        end
+        
+        local text = "[ ".. os.date() .." ]".. message
+        
+        table.insert(log._messages, text)
+    end
+} 
+
+return setmetatable(log, {
+    __call = function(_, ...)
+        log.add(...)
+    end
+})
